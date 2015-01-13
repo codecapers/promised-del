f893b3072c522a74e126909572ded6ef8561bc64
Ashley Davis <ashley.davis@realseriousgames.com>
2015-01-13 15:57:48 +1000

    Now defaulting options to an empty object when not supplied.

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var Promise = require('promise');
 //
 module.exports = function (patterns, options) {
 	return new Promise(function (resolve, reject) {
-		del(patterns, options, function (err, deletedFiles) {
+		del(patterns, options || {}, function (err, deletedFiles) {
 			if (err) {
 				reject(err);
 			}
